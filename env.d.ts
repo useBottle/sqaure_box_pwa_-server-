b816cefd001d41d44354bfe751974d24e413345d
Jeong Byeong Ho <zero8660@gmail.com>
2024-05-16 01:15:25 +0900

    실시간 검색어를 크롤링하고 클라이언트로 실시간 전송을 하기 위해 메인 서버 및 라우터 기초 세팅 완료.

--- env.d.ts
+++ env.d.ts
@@ -4,5 +4,6 @@ declare namespace NodeJS {
     REQUEST_DOMAIN: string;
     CLIENT_ID: string;
     CLIENT_SECRET: string;
+    LIVE_KEYWORDS_URL: string;
   }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
 import getNewsAPI from "./routes/getNewsAPI.js";
+import getLiveKeywordsAPI from "./routes/getLiveKeywords.js";
+import { createServer } from "http";
+import { Server as SocketIOServer } from "socket.io";
+import { setupKeywordsRoutes } from "./routes/getLiveKeywords.js";
 
 dotenv.config();
 
 const app: Application = express();
+const httpServer = createServer(app);
+const io = new SocketIOServer(httpServer);
 const port: number = Number(process.env.PORT as string) || 8080;
-
-app.listen(port, (): void => {
-  console.log(`Running server on http://localhost:${port}`);
-});
 
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", (req: Request, res: Response): void => {
-  res.send("Hello World!");
-});
+setupKeywordsRoutes(app, io);
 
 const corsOptions = {
   origin: process.env.REQUEST_DOMAIN as string,
@@ -27,3 +27,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use("/getNewsAPI", getNewsAPI);
+app.use("/getLiveKeywordsAPI", getLiveKeywordsAPI);
+
+httpServer.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default io;
