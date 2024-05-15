8510a65643a5b7bfc0259453512a194a142813ca
Jeong Byeong Ho <zero8660@gmail.com>
2024-05-16 00:31:05 +0900

    실시간 검색어 제공 사이트에서 필요한 데이터 크롤링하는 로직 추가.

--- src/routes/getLiveKeywords.ts
+++ src/routes/getLiveKeywords.ts
@@ -10,33 +10,46 @@ const router = express.Router();
 export const setupKeywordsRoutes = (app: Application, io: SocketIOServer) => {
   const router = Router();
 
-  router.put("/", async (req: Request, res: Response): Promise<void> => {
-    // 데이터 처리 로직
-    io.on("connection", (socket) => {
-      console.log("New client connected");
+  router.get("/", async (req: Request, res: Response): Promise<void> => {
+    if (req.method === "GET") {
+      // 데이터 처리 로직
+      io.on("connection", (socket) => {
+        console.log("New client connected");
 
-      const intervalId = setInterval(async () => {
-        try {
-          const response = await axios.get(process.env.LIVE_KEYWORDS_URL);
-          const data = response.data;
-          socket.emit("data", data);
-        } catch (error: any) {
-          if (axios.isAxiosError(error)) {
-            const axiosError = error as AxiosError;
-            res.status(axiosError.response?.status ?? 500).send();
-            console.error("error =", axiosError.response?.status);
-          } else {
-            res.status(500).send("An unexpected error occurred");
-            console.error("Non-axios error occurred", error);
+        const intervalId = setInterval(async () => {
+          try {
+            const response = await axios.get(process.env.LIVE_KEYWORDS_URL);
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const number = $(".container .realtime-rank .rank-column .rank-layer .rank-num").text();
+            const keyword = $(".container .realtime-rank .rank-column .rank-layer .rank-text").text();
+            const iconClassName = $(".container .realtime-rank .rank-column .rank-layer .rank-icon").attr("class");
+            const data = {
+              number: number,
+              keyword: keyword,
+              iconClassName: iconClassName,
+            };
+            socket.emit("data", data);
+          } catch (error: any) {
+            if (axios.isAxiosError(error)) {
+              const axiosError = error as AxiosError;
+              res.status(axiosError.response?.status ?? 500).send();
+              console.error("error =", axiosError.response?.status);
+            } else {
+              res.status(500).send("An unexpected error occurred");
+              console.error("Non-axios error occurred", error);
+            }
           }
-        }
-      }, 1000);
+        }, 60000);
 
-      socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        clearInterval(intervalId);
+        socket.on("disconnect", () => {
+          console.log("Client disconnected");
+          clearInterval(intervalId);
+        });
       });
-    });
+    } else {
+      res.status(302).json({ message: "This is an incorrect approach." });
+    }
     res.status(200).json({ message: "Data has been transferred." });
   });
 
