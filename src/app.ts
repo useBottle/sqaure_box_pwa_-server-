26ebd2b813cd337892699912d0a03d18a7f39bb4
Jeong Byeong Ho <zero8660@gmail.com>
2024-05-21 21:21:40 +0900

    디펜던시 복구

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,17 @@ import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
 import getNewsAPI from "./routes/getNewsAPI.js";
-import getLiveKeywordsAPI from "./routes/getLiveKeywords.js";
-import { createServer } from "http";
-import { Server as SocketIOServer } from "socket.io";
-import { setupKeywordsRoutes } from "./routes/getLiveKeywords.js";
 
 dotenv.config();
 
 const app: Application = express();
-const httpServer = createServer(app);
-const io = new SocketIOServer(httpServer);
 const port: number = Number(process.env.PORT as string) || 8080;
+app.listen(port, (): void => {
+  console.log(`Running server on http://localhost:${port}`);
+});
 
 app.use(express.json());
 app.use(morgan("dev"));
-
-setupKeywordsRoutes(app, io);
 
 const corsOptions = {
   origin: process.env.REQUEST_DOMAIN as string,
@@ -27,10 +22,3 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use("/getNewsAPI", getNewsAPI);
-app.use("/getLiveKeywordsAPI", getLiveKeywordsAPI);
-
-httpServer.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-export default io;
