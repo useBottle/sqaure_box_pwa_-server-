bef3e524b9d6280d6962afe6b8ffea8b3b70ec34
Jeong Byeong Ho <zero8660@gmail.com>
2024-06-13 19:24:34 +0900

    사용자 패스워드 해시 처리 및 유효성 검사 생성.

--- src/routes/signUp.ts
+++ src/routes/signUp.ts
@@ -1,21 +1,38 @@
 import axios, { AxiosError } from "axios";
 import express, { Request, Response } from "express";
 import { connectDB } from "../database.js";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
 router.post("/", async (req: Request, res: Response): Promise<Response> => {
   const { idValue, passwordValue } = req.body;
+  const idPattern = /^[A-Za-z0-9]{6,20}$/;
+  const passwordPattern = /^(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{6,}$/;
+  const idValid = idPattern.test(idValue);
+  const passwordValid = passwordPattern.test(passwordValue);
 
-  if (idValue === "") {
-    return res.status(500).json("ID is empty.");
-  } else if (passwordValue === "") {
-    return res.status(500).json("Password is empty.");
+  const user = {
+    id: "",
+    password: "",
+  };
+
+  if (idValue && idValid) {
+    user.id = idValue;
+  } else if (!idValue || !idValid) {
+    return res.status(500).json("ID is empty or invalid.");
+  }
+
+  if (passwordValue && passwordValid) {
+    const hashedPassword = await bcrypt.hash(passwordValue, 10);
+    user.password = hashedPassword;
+  } else if (!passwordValue || !passwordValid) {
+    return res.status(500).json("Password is empty or invalid.");
   }
 
   try {
     const db = (await connectDB).db("Square_Board");
-    const result = db.collection("users").insertOne(req.body);
+    const result = db.collection("users").insertOne(user);
     return res.status(200).json(result);
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
