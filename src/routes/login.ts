ecd16ccc3b6cac777e6d25609ee291480754ca80
Jeong Byeong Ho <zero8660@gmail.com>
2024-06-16 19:56:02 +0900

    JWT 방식 로그인 서버 생성.

---
+++ src/routes/login.ts
@@ -0,0 +1,50 @@
+import bcrypt from "bcrypt";
+import express, { Request, Response } from "express";
+import { connectDB } from "../database.js";
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import authenticateJWT, { CustomRequest } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.put("/", async (req: Request, res: Response): Promise<Response | void> => {
+  const { idValue, passwordValue } = req.body;
+
+  if (!idValue || !passwordValue) {
+    return res.status(400).json("ID or password is empty.");
+  }
+
+  try {
+    await connectDB;
+    const userInfo = await User.findOne({ id: idValue });
+
+    if (!userInfo) {
+      return res.status(404).json("User not found.");
+    }
+
+    const isPasswordValid = await bcrypt.compare(passwordValue, userInfo.password);
+    if (!isPasswordValid) {
+      return res.status(401).json("Invalid password");
+    }
+
+    if (!process.env.ACCESS_TOKEN_SECRET) {
+      return res.status(500).json("ACCESS_TOKEN_SECRET is not defined");
+    }
+
+    const token = jwt.sign({ username: idValue }, process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: "1h" });
+    const refreshToken = jwt.sign({ username: idValue }, process.env.REFRESH_TOKEN_SECRET as string, {
+      expiresIn: "7d",
+    });
+    return res.status(200).json({ token, refreshToken });
+  } catch (error: unknown) {
+    console.error(error);
+    return res.status(500).send("An unexpected error occurred");
+  }
+});
+
+router.get("/login", authenticateJWT, (req: Request, res: Response) => {
+  const customReq = req as CustomRequest;
+  res.json({ message: `Hello, ${customReq.user.username}` });
+});
+
+export default router;
