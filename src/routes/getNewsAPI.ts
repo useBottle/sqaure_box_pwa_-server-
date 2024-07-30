093d10cb15c2f6d9d9e8da4cbe567d57f0239fdc
Jeong Byeong Ho <zero8660@gmail.com>
2024-07-30 18:20:16 +0900

    getNewsAPI 주석 첨부.

--- src/routes/getNewsAPI.ts
+++ src/routes/getNewsAPI.ts
@@ -15,6 +15,7 @@ const client_secret = process.env.NAVER_API_CLIENT_SECRET;
 const numberOfArticles = 10;
 const wayOfSort = ["sim", "date"];
 
+// 뉴스 기사 본문에 씌워진 HTML 태그 제거.
 const stripHtml = (html: string): string => {
   const dom = new JSDOM();
   const document = dom.window.document;
@@ -23,6 +24,7 @@ const stripHtml = (html: string): string => {
   return tempDiv.textContent || tempDiv.innerText;
 };
 
+// 뉴스 기사 본문 텍스트 양식 정리.
 const cleanText = (text: string): string | void => {
   try {
     // 빈 줄을 기준으로 텍스트를 분할
@@ -60,11 +62,13 @@ router.put("/", async (req: Request, res: Response): Promise<void> => {
 
     const data = response.data.items;
 
+    // API 에서 가져온 각각의 소스를 병렬 처리.
     const articleContents = await Promise.all(
       data.map(async (item: Item) => {
         const dateString = item.pubDate;
         const date = new Date(dateString);
 
+        // 날짜 포맷 변경
         function formatDate(d: Date): string {
           const year = d.getFullYear();
           const month = d.getMonth() + 1;
@@ -74,8 +78,9 @@ router.put("/", async (req: Request, res: Response): Promise<void> => {
           const pad = (num: number) => num.toString().padStart(2, "0");
           return `${year}년 ${pad(month)}월 ${pad(day)}일 ${pad(hours)}:${pad(minutes)}`;
         }
-        const changedDate = formatDate(date);
 
+        // 정제된 데이터 저장.
+        const changedDate = formatDate(date);
         const imageUrls: string[] = [];
         let articleText;
         const title = stripHtml(item.title);
@@ -84,6 +89,7 @@ router.put("/", async (req: Request, res: Response): Promise<void> => {
         const originallink = stripHtml(item.originallink);
         const link = stripHtml(item.link);
 
+        // 뉴스 기사 본문 텍스트 및 이미지 추출 요청.
         const fetchData = async (url: string): Promise<string | void> => {
           try {
             const response = await axios.get(url, { responseType: "arraybuffer" });
@@ -114,6 +120,7 @@ router.put("/", async (req: Request, res: Response): Promise<void> => {
 
             const metaTags = $("meta");
 
+            // 뉴스 기사 이미지 경로 추출
             metaTags.each((_: number, tag: cheerio.Element) => {
               const contentValue = $(tag).attr("content");
               if (contentValue && imagePattern.test(contentValue)) {
@@ -126,11 +133,13 @@ router.put("/", async (req: Request, res: Response): Promise<void> => {
             const dom = new JSDOM(decodedData);
             const document = dom.window.document;
 
+            // 기사 본문에 불필요한 요소들 제거.
             const tagToRemove = document.querySelectorAll(
               "h1, h2, h3, h4, h5, h6, .heading, .title, a, span, ul, li, table, figcaption, .reveal-container, .date-repoter, .copy_info, .reaction_btn_wrap, .option_group, .v_topimg_wrap, .divtext2, .inner-subtitle, .news_write_info_group, .photojournal, .article_summary, .summary_area, .article-head-sub, .caption, .writer, .subtitle, .view-info, .cnt_title_wrap2, .aboutPhoto, .expendImageWrap, .precis",
             );
             tagToRemove.forEach((link) => link.parentNode?.removeChild(link));
 
+            // 불필요한 요소 제거 후 남은 요소 중에서 Readability 라이브러리로 뉴스 기사 본문 자동 추출.
             const reader = new Readability(document);
             const article = reader.parse();
             const encodedText = article ? cleanText(stripHtml(article.textContent)) : null;
@@ -140,6 +149,7 @@ router.put("/", async (req: Request, res: Response): Promise<void> => {
           }
         };
 
+        // 최대한 데이터 추출 실패를 방지하기 위해 두 링크 모두 순회하여 추출 시도.
         for (const url of [item.originallink, item.link]) {
           await fetchData(url);
           if (imageUrls.length > 0) break;
