05eeeedbb2e6798edd23003a872fce7ada906dca
Jeong Byeong Ho <zero8660@gmail.com>
2024-08-14 20:38:37 +0900

    access 토큰 유무에 따라 인증 로직 조건문 수정.

--- src/routes/checkToken.ts
+++ src/routes/checkToken.ts
@@ -6,15 +6,14 @@ const router = express.Router();
 router.get("/", (req: Request, res: Response) => {
   const accessToken = req.cookies.accessToken;
 
-  if (!accessToken) {
-    return res.status(403).json("A token is required for authentication");
-  }
-
-  const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET as string) as JwtPayload;
-
   try {
-    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET as string);
-    return res.status(200).json({ message: "Access token is valid.", username: decoded.username });
+    if (!accessToken) {
+      return res.status(403).json("A token is required for authentication");
+    } else if (accessToken) {
+      const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET as string) as JwtPayload;
+      jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET as string);
+      return res.status(200).json({ message: "Access token is valid.", username: decoded.username });
+    }
   } catch (error) {
     console.error("An unexpected error occurred", error);
     return res.status(401).json("Invalid token");
