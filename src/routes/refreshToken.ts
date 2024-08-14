422dce7a3866ef3478414f98f72e0345d8ca33a1
Jeong Byeong Ho <zero8660@gmail.com>
2024-08-14 20:40:27 +0900

    리프레시 토큰 유무에 따른 인증 로직 조건문 수정.

--- src/routes/refreshToken.ts
+++ src/routes/refreshToken.ts
@@ -6,26 +6,26 @@ const router = express.Router();
 router.get("/", (req: Request, res: Response) => {
   const refreshToken = req.cookies.refreshToken;
 
-  if (!refreshToken) {
-    return res.status(401).json("Refresh token is required.");
-  }
-
   try {
-    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as string) as JwtPayload;
-    const accessToken = jwt.sign({ username: decoded.username }, process.env.ACCESS_TOKEN_SECRET as string, {
-      expiresIn: "1h",
-    });
+    if (!refreshToken) {
+      return res.status(401).json("Refresh token is required.");
+    } else if (refreshToken) {
+      const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as string) as JwtPayload;
+      const accessToken = jwt.sign({ username: decoded.username }, process.env.ACCESS_TOKEN_SECRET as string, {
+        expiresIn: "1h",
+      });
 
-    const isProduction = process.env.NODE_ENV === "production";
+      const isProduction = process.env.NODE_ENV === "production";
 
-    // 실행 환경에 따라 토큰 보안 설정 변경.
-    res.cookie("accessToken", accessToken, {
-      httpOnly: isProduction,
-      secure: isProduction,
-      sameSite: isProduction ? "none" : "lax",
-      maxAge: 3600000,
-    });
-    res.status(200).json({ message: "Access token refreshed successfully", username: decoded.username });
+      // 실행 환경에 따라 토큰 보안 설정 변경.
+      res.cookie("accessToken", accessToken, {
+        httpOnly: isProduction,
+        secure: isProduction,
+        sameSite: isProduction ? "none" : "lax",
+        maxAge: 3600000,
+      });
+      res.status(200).json({ message: "Access token refreshed successfully", username: decoded.username });
+    }
   } catch (error) {
     console.error("An unexpected error occurred");
     return res.status(403).json("Invalid refresh token.");
